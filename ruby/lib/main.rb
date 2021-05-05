3085a11e40a51f5efb98fbc05651bf13b5e67d5f
Thomas Marlow <thomasmarlow.ds@gmail.com>
2021-05-05 18:50:01 -0300

    save! ahora permite atributos nulos y funciona cuando la instancia ya había sido guardada

--- ruby/lib/main.rb
+++ ruby/lib/main.rb
@@ -1,10 +1,9 @@
 require 'tadb'
 
-
 class Module
     def has_one type, description
         # TODO check tipos
-        extend PersistibleModule
+        extend ORM::PersistibleModule
         initialize_persistence
         attr_name = description[:named]
         @persistible_attrs.delete_if { |attr| attr[:name] == attr_name } 
@@ -14,57 +13,66 @@ class Module
 end 
 
 
-module PersistibleObject
-    def save!
-        if not @id
-            define_singleton_method(:id) { @id } # en la singleton class porque sólo tienen que tenerlo los objetos a los que se les mandó save!
-            self_hashed = {} # TODO seguramente haya alguna forma de hacer esto más bonito pero anda
-            (self.class.instance_variable_get :@persistible_attrs).each do |attr|
-                selector = attr[:name]
-                self_hashed[selector] = self.send selector # incluyo cada atributo a persistir en el hash que mando a insertar a la tabla
+module ORM
+    module PersistibleObject
+        def save!
+            if not @id
+                define_singleton_method(:id) { @id } # en la singleton class porque sólo tienen que tenerlo los objetos a los que se les mandó save!
+                self_hashed = {} # TODO seguramente haya alguna forma de hacer esto más bonito pero anda
+                (self.class.instance_variable_get :@persistible_attrs).each do |attr|
+                    attr_value = send attr[:name]
+                    if attr_value != nil then self_hashed[attr[:name]] = attr_value end # para no romper en caso de que un atributo esté vacío, por ahora
+                end
+                @id = self.class.table_insert self_hashed
+                return # para no devolver el id afuera
             end
-            @id = self.class.table_insert self_hashed
-        else
-            # TODO pensar cómo conviene hacer updates (por el tema del id autogenerado)
+            forget! # TADB sólo permite insertar o borrar, no hay update. así que...
+            save!
+        end
+
+        def refresh!
+            (self.class.table_find_by_id @id).each do |attr_name, attr_value|
+                if attr_name != :id # necesito hacer esto porque :id no tiene setter; no hago un reject de antemano porque sería menos performante creo
+                    send (attr_name.to_s + '=').to_sym, attr_value
+                end
+            end
+        end
+
+        def forget!
+            self.class.table_delete_by_id @id
+            singleton_class.remove_method :id
+            @id = nil # si no hago esto, el id viejo queda volando adentro del objeto y al hacer un nuevo save! se rompe 
         end
     end
 
-    def refresh!
-        (self.class.table_find_by_id @id).each do |attr_name, attr_value|
-            if attr_name != :id # necesito hacer esto porque :id no tiene setter; no hago un reject de antemano porque sería menos performante creo
-                self.send (attr_name.to_s + '=').to_sym, attr_value
+
+    module PersistibleModule
+        def initialize_persistence
+            if not @table
+                include PersistibleObject
+                @persistible_attrs = []
+                @table = TADB::DB.table(name)
             end
         end
-    end
 
-    def forget!
-        self.class.table_delete_by_id @id
-        singleton_class.remove_method :id
-        @id = nil # si no hago esto, el id viejo queda volando adentro del objeto y al hacer un nuevo save! se rompe 
+        def table_insert hashed_instance
+            @table.insert(hashed_instance)
+        end      
+
+        def table_find_by_id id
+            @table.entries.detect { |entry| entry[:id] == id }
+        end
+
+        def table_delete_by_id id
+            @table.delete id
+        end
     end
 end
 
-
-module PersistibleModule
-    def initialize_persistence
-        if not @table
-            include PersistibleObject
-            @persistible_attrs = []
-            @table = TADB::DB.table(name)
-        end
-    end
-
-    def table_insert hashed_instance
-        @table.insert(hashed_instance)
-    end      
-
-    def table_find_by_id id
-        @table.entries.detect { |entry| entry[:id] == id }
-    end
-
-    def table_delete_by_id id
-        @table.delete id
+# para testear a manopla
+module CosasTesting
+    class A
+        has_one String, named: :coso
+        has_one Numeric, named: :coso
     end
 end
-
-
