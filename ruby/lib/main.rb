38e8e1c902dfd8507f4d99e6ac0b0efc021ac0d1
Thomas Marlow <thomasmarlow.ds@gmail.com>
2021-05-09 03:40:44 -0300

    implemento 3.a y 3.b, pero hay que terminar la lógica de los deletion observers para que el forget y el refresh anden bien siempre

--- ruby/lib/main.rb
+++ ruby/lib/main.rb
@@ -2,23 +2,44 @@ require 'tadb'
 
 
 class Module
-    def has_one type, description
+    def ORM_add_persistible_attr type, description, is_multiple
+        if type.ancestors.include? ORM::PersistibleObject
+            type.send :ORM_add_deletion_observer, self
+        end
         if not @table
             extend ORM::PersistibleModule # así el módulo/clase soporta persistencia (tiene tabla, atributos persistibles, etc.)
-            include ORM::PersistibleObject # para que los objetos tengan el comportamiento de persistencia
-            @persistible_attrs = [{name: :id, type: String}]
+            prepend ORM::PersistibleObject # para que los objetos tengan el comportamiento de persistencia
+            @deletion_observers = []
+            @persistible_attrs = [{name: :id, type: String, multiple: false}]
             @table = TADB::DB.table(name)
         end
         attr_name = description[:named]
         @persistible_attrs.delete_if { |attr| attr[:name] == attr_name } 
-        @persistible_attrs << {name: attr_name, type: type} # @persistible_attrs sería como la metadata de la @table del módulo
+        @persistible_attrs << {name: attr_name, type: type, multiple: is_multiple} # @persistible_attrs sería como la metadata de la @table del módulo
         attr_accessor attr_name # define getters+setters para los objetos
     end
+
+    def has_one type, description
+        ORM_add_persistible_attr type, description, false
+    end
+
+    def has_many type, description
+        ORM_add_persistible_attr type, description, true
+    end
+
+    private :ORM_add_persistible_attr
 end 
 
 
 module ORM # a las cosas de acá se puede acceder a través de ORM::<algo>; la idea es no contaminar el namespace
     module PersistibleObject # esto es sólo para objetos; todo lo estático está en PersistibleModule
+        def initialize *args
+            ((self.class.instance_variable_get :@persistible_attrs).select { |attr| attr[:multiple] }).each do |attr|
+                send (attr[:name].to_s + '=').to_sym, []
+            end
+            super *args
+        end
+
         def save!
             if @id 
                 self.class.send :ORM_delete_entry, @id
@@ -26,41 +47,60 @@ module ORM # a las cosas de acá se puede acceder a través de ORM::<algo>; la i
                 define_singleton_method(:id) { @id }
             end
             self_hashed = {} # TODO seguramente haya alguna forma de hacer esto más bonito pero anda
-            (self.class.instance_variable_get :@persistible_attrs).each do |attr|
+            ((self.class.instance_variable_get :@persistible_attrs).reject { |attr| attr[:multiple] }).each do |attr|
                 attr_value = send attr[:name]
                 if not attr_value == nil
                     if attr[:type].ancestors.include? PersistibleObject # TODO abstraer?
-                        attr_final_value = attr_value.save!
+                        self_hashed[attr[:name]] = attr_value.save!
                     else
-                        attr_final_value = attr_value
+                        self_hashed[attr[:name]] = attr_value
                     end
-                    self_hashed[attr[:name]] = attr_final_value
                 end
             end
             @id = self.class.send :ORM_insert, self_hashed
+            ((self.class.instance_variable_get :@persistible_attrs).select { |attr| attr[:multiple] }).each do |attr|
+                id_pair_hashed = {}
+                id_pair_hashed[('id_' + self.class.name).to_sym] = @id
+                (send attr[:name]).each do |elem|
+                    id_pair_hashed[('id_' + attr[:name].to_s).to_sym] = elem.save!
+                    (attr_table attr[:name]).insert id_pair_hashed
+                end
+            end
+            return @id
         end
 
         def refresh!
             exception_if_no_id
-            (self.class.send :ORM_get_entry, @id).each do |attr_name, attr_value|
-                if attr_name != :id # necesito hacer esto porque :id no tiene setter; no hago un reject de antemano porque sería menos performante creo
-                    attr_type = ((self.class.instance_variable_get :@persistible_attrs).detect{ |attr| attr[:name] == attr_name })[:type] # TODO abstraer
-                    if attr_type.singleton_class.ancestors.include? PersistibleModule # TODO abstraer?
-                        attr_final_value = (attr_type.find_by_id attr_value)[0] # TODO ojo porque lo estamos buscando desde la tabla; podría haber varios objetos distintos para la misma entrada de la tabla
-                    else
-                        attr_final_value = attr_value
-                    end
-                    send (attr_name.to_s + '=').to_sym, attr_final_value # seteo cada atributo con el valor dado por la entry de la tabla
+            entry = self.class.send :ORM_get_entry, @id
+            ((self.class.instance_variable_get :@persistible_attrs).reject { |attr| attr[:multiple] or attr[:name] == :id}).each do |attr| # TODO abstraer
+                if attr[:type].ancestors.include? PersistibleObject
+                    attr_final_value = (attr[:type].find_by_id entry[attr[:name]])[0]
+                else
+                    attr_final_value = entry[attr[:name]]
                 end
+                send (attr[:name].to_s + '=').to_sym, attr_final_value # seteo cada atributo con el valor dado por la entry de la tabla
+            end
+            ((self.class.instance_variable_get :@persistible_attrs).select { |attr| attr[:multiple] }).each do |attr|
+                matching_entries = ((attr_table attr[:name]).entries.select { |entry| entry[('id_' + self.class.name).to_sym] == @id })
+                elem_enum = matching_entries.map do |entry|
+                    attr[:type].find_by_id entry[('id_' + attr[:name].to_s).to_sym]
+                end
+                send (attr[:name].to_s + '=').to_sym, elem_enum.to_a
             end
             self
         end
 
+        # TODO el forget tiene que borrar toda referencia al id en otras tablas!!!
         def forget! # se asume que no cascadea
             exception_if_no_id
+            self.class.send :ORM_notify_deletion, @id
             self.class.send :ORM_delete_entry, @id
             singleton_class.remove_method :id
             @id = nil # si no hago esto, el id viejo queda volando adentro del objeto y al hacer un nuevo save! puede romper
+        end
+
+        def attr_table attr_name_symbol 
+            TADB::DB.table(self.class.name + '__' + attr_name_symbol.to_s)
         end
 
         def exception_if_no_id
@@ -84,6 +124,29 @@ module ORM # a las cosas de acá se puede acceder a través de ORM::<algo>; la i
             @table.delete id
         end
 
+        def ORM_notify_deletion id
+            @deletion_observers.each do |observer|
+                observer.send :ORM_wipe_references_to, self, id
+            end
+        end
+
+        def ORM_wipe_references_to type, id
+            (@persistible_attrs.select { |attr| attr[:type] == type }).each do |attr|
+                if attr[:multiple]
+                    # TODO
+                else
+                    intances_to_update = send ('find_by_' + attr[:name].to_s).to_sym, id
+                    intances_to_update.each do |instance|
+                        # TODO
+                    end
+                end
+            end
+        end
+
+        def ORM_add_deletion_observer a_class
+            @deletion_observers << a_class
+        end
+
         def all_instances 
             instantiate @table.entries
         end
@@ -92,6 +155,7 @@ module ORM # a las cosas de acá se puede acceder a través de ORM::<algo>; la i
             prefix = 'find_by_'
             query_attr = @persistible_attrs.detect { |attr| attr[:name].to_s == symbol.to_s[(prefix.length)..-1] } # buscamos el campo según el cual se filtra en la query
             if query_attr and symbol.to_s.start_with? prefix # si el campo existe y el prefijo es el correcto:
+                # TODO no se está contemplando comparar por id para los atributos persistibles referenciados
                 instantiate @table.entries.select { |entry| entry[query_attr[:name]] == args[0] } # se instancian los que cumplen la condición
             else
                 super # si no matchea, continúa el method lookup
@@ -109,20 +173,25 @@ module ORM # a las cosas de acá se puede acceder a través de ORM::<algo>; la i
             end 
         end
 
-        private :instantiate, :ORM_insert, :ORM_get_entry, :ORM_delete_entry
+        private :instantiate, :ORM_insert, :ORM_get_entry, :ORM_delete_entry, :ORM_notify_deletion, :ORM_wipe_references_to, :ORM_add_deletion_observer
     end
 end
 
 
 # para testear a manopla
 module CosasTesting
+    class DNI
+        has_one Numeric, named: :number
+    end
+
     class Grade
         has_one Numeric, named: :value
     end
 
     class Student
         has_one String, named: :full_name
-        has_one Grade, named: :grade
+        has_one DNI, named: :dni
+        has_many Grade, named: :grades
     end
 end
 
