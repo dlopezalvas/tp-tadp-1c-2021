d60cfb129cb5ad72e3e915c99136ed5563c00ea5
Diana Lopez Alvas <dlopezalvas@gmail.com>
2021-05-10 22:35:22 -0300

    tests save!

--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -18,9 +18,10 @@ describe "Persistencia de objetos sencillos" do
   let(:persona) { Person.new }
 
   describe 'has_one' do
-    it 'un objeto persistible puede cambiar el valor' do
+    it 'un atributo de un objeto persistible puede cambiar el valor multiples veces' do
       persona.first_name = "raul"
-      expect(persona.first_name).to eq "raul"
+      persona.first_name = "jorge"
+      expect(persona.first_name).to eq "jorge"
     end
 
     it 'si defino dos atributos con el mismo nombre, el segundo pisa el primero' do
@@ -28,6 +29,21 @@ describe "Persistencia de objetos sencillos" do
       Person.has_one String, named: :address
       persona.address = "Av. San Martin 2567"
       expect(persona.address.class).to be String
+    end
+  end
+
+  describe 'save!' do
+    it 'al guardar un objeto persistible adquiere un atributo persistible @id' do
+      persona.first_name = "raul"
+      persona.last_name = "porcheto"
+      persona.save!
+      expect(persona.id.class).to be String
+    end
+
+    it 'un objeto persistible sin guardar no tiene un @id' do
+      persona.first_name = "raul"
+      persona.last_name = "porcheto"
+      expect{persona.id}.to raise_error NoMethodError
     end
   end
 end
