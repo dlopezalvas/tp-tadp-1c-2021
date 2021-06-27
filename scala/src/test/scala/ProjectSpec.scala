43455ae9b3290c32b092d49b31cff570485dc166
Nicolas Bordon <nbordon91@gmail.com>
2021-06-26 22:38:42 -0300

    un par de test

--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -1,15 +1,58 @@
+import Tipos._
 import org.scalatest.matchers.should.Matchers._
-import org.scalatest.freespec.{AnyFreeSpec}
+import org.scalatest.freespec.AnyFreeSpec
 
 class ProjectSpec extends AnyFreeSpec {
 
-  "Este proyecto" - {
+  "Jugadas" - {
 
     "cuando está correctamente configurado" - {
       "debería resolver las dependencias y pasar este test" in {
-        Prueba.materia shouldBe "tadp"
+        1 shouldBe 1
+      }
+    }
+
+    "Moneda" - {
+      "Probabilidad que salga Cara debe ser 50%" in {
+        val jugarACara:JugadaMoneda = JugadaMoneda(LadoMoneda.Cara)
+
+        Moneda.getDDPJugadas(List(jugarACara)) shouldBe DDPJugadas(List(
+          (List(JugadaMoneda(LadoMoneda.Cara)),0.5),
+          (List(),0.5)
+        ))
+      }
+
+      "Si juego $10 a cara tengo 50% de probabilidad de ganar $20" in {
+        val jugarACara:JugadaMoneda = JugadaMoneda(LadoMoneda.Cara)
+        val apuesta:Apuesta = List((jugarACara,10.0))
+        Moneda.getDDPApuesta(apuesta) shouldBe DDPApuesta(List(
+          (20.0,0.5),
+          (0.0,0.5)
+        ))
+      }
+
+      "Si juego $10 a Rojo, $5 a Par en la ruleta" in {
+        val jugarARojo:JugadaRuleta = ColorJugado(Color.Rojo)
+        val jugarAPar:JugadaRuleta = ParidadJugada(Paridad.Par)
+        val apuesta:Apuesta = List((jugarARojo,10.0),(jugarAPar,5.0))
+        Ruleta.getDDPApuesta(apuesta) shouldBe DDPApuesta(List(
+          ( 0.0, 9.0/37.0),
+          (20.0, 10.0/37.0),
+          (10.0, 10.0/37.0),
+          (30.0, 8.0/37.0)
+        ))
+      }
+
+      "Si juego $12 a Rojo, $12 a Par en la ruleta" in {
+        val jugarARojo:JugadaRuleta = ColorJugado(Color.Rojo)
+        val jugarAPar:JugadaRuleta = ParidadJugada(Paridad.Par)
+        val apuesta:Apuesta = List((jugarARojo,12.0),(jugarAPar,12.0))
+        Ruleta.getDDPApuesta(apuesta) shouldBe DDPApuesta(List(
+          ( 0.0, 9.0/37.0),
+          (24.0, 20.0/37.0),
+          (48.0, 8.0/37.0)
+        ))
       }
     }
   }
-
 }
