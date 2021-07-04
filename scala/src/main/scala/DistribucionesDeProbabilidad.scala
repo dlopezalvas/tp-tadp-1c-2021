95747dbb7f2613c121b168ccc2a918c328e00732
unknown <candela.sierra98@gmail.com>
2021-07-04 20:51:05 -0300

    refactor metodos distribucion probabilidad

--- scala/src/main/scala/DistribucionesDeProbabilidad.scala
+++ scala/src/main/scala/DistribucionesDeProbabilidad.scala
@@ -3,69 +3,44 @@ import Tipos.{Apuesta, Dinero, Probabilidad}
 
 // DistribucionParaJugadas: un wrapper para la distribución cruda
 case class DistribucionParaJugadas(distribucion: List[(List[Jugada], Probabilidad)]) {
-
-  // este método es medio feo y habría que rehacerlo; se entiende en las últimas 4 líneas
-  // TODO: refactor para deshacerse de ese flag feo
+  
   def incrementarProbabilidadDe(combinacionDeJugadas: List[Jugada], probabilidadASumar: Probabilidad): DistribucionParaJugadas = {
-    var laCombinacionDeJugadasEstabaContempladaEnLaLista = false
-    val distribucionConUnaProbabilidadIncrementada = DistribucionParaJugadas(
-      distribucion.map {
-        case (jugadas, probabilidad) if (jugadas.toSet == combinacionDeJugadas.toSet) => {
-          laCombinacionDeJugadasEstabaContempladaEnLaLista  = true
-          (jugadas, probabilidad + probabilidadASumar)
-        }
-        case x => x
-      }
-    )
-    if (laCombinacionDeJugadasEstabaContempladaEnLaLista) // si existe ya la comb. de jugadas, solo se suma la prob.
-      distribucionConUnaProbabilidadIncrementada
-    else // si no, se agrega una nueva tupla para esa comb. de jugadas
-      DistribucionParaJugadas(distribucion.appended((combinacionDeJugadas, probabilidadASumar)))
+    if(existe(combinacionDeJugadas)){
+      DistribucionParaJugadas(distribucion.map {
+          case (jugadas, probabilidad) if (jugadas.toSet == combinacionDeJugadas.toSet) => (jugadas, probabilidad + probabilidadASumar)
+          case x => x
+        })
+    }
+    DistribucionParaJugadas(distribucion.appended((combinacionDeJugadas, probabilidadASumar)))
   }
+
+  def existe(combinacionDeJugadas: List[Jugada]) =
+    distribucion.exists{case (jugadas, _) => (jugadas.toSet == combinacionDeJugadas.toSet)}
 }
 
 
 // DistribucionParaApuestas: un wrapper para la distribución cruda
 case class DistribucionParaApuestas(distribucion: List[(Dinero, Probabilidad)]) {
-  /*def combinar(apuesta: Apuesta): DistribucionParaApuestas ={
+  def combinar(apuesta: Apuesta): DistribucionParaApuestas ={
     var nuevaDistribucion :List[(Dinero,Probabilidad)] = List.empty
     distribucion.foreach{
-      case (dineroOriginal, probabilidad) if(dinero >= apuesta.montoTotalApostado) =>
+      case (dineroOriginal, probabilidad) if(dineroOriginal >= apuesta.montoTotalApostado) =>
         apuesta.simular().forEach{ case (dineroGanado, probabilidadDineroGanado)
           nuevaDistribucion.add((dineroOriginal - apuesta.montoTotalApostado + dineroGanado), (probabilidad * probabilidadDineroGanado))
         }
       case _ => this
     }
     //Cosa magica que elimina repetidos y junta las probabilidades o ver en el add si ya existe.
-  }*/
+  }
 }
 case object DistribucionParaApuestas {
   def unificarRepetidos(distribucion: List[(Dinero, Probabilidad)]): List[(Dinero, Probabilidad)] = {
-    // primero necesita obtener los montos (dinero) como claves únicas (descartando los repetidos con toSet)
-    val dineroSinRepetidos = distribucion.map(_ match { case (dinero, _) => dinero }).toSet
-    // después mapea cada monto/dinero a su correspondiente par (dinero, probabilidadTotalDeEseDinero)
-    dineroSinRepetidos.map( dinero =>
-      (
-        dinero,
-        ( // probabilidadTotalDeEseDinero:
-          distribucion filter { case (dinero_, _) => dinero == dinero_ }
-                       map    { case (_, probabilidad_) => probabilidad_ }
-        ).sum // es la suma de las probabilidades de ese monto/dinero
-      )
-    ).toList // convierte el Set a List para devolver el tipo correcto
+    val dineroSinRepetidos = distribucion.map(_._1).toSet
+    dineroSinRepetidos.map(dinero => (dinero, distribucion.filter(_._1 == dinero).map(_._2).sum)).toList
   }
 
   def desdeDistribucionParaJugadas(distribucionParaJugadas: DistribucionParaJugadas, apuestas: List[Apuesta]) : DistribucionParaApuestas = {
-    DistribucionParaApuestas(unificarRepetidos(
-      distribucionParaJugadas
-        .distribucion
-        .map { // mapea cada conjunto de jugadas a su correspondiente dinero según las apuestas dadas
-          case (jugadas, probabilidad) => (
-            jugadas.map(_.dineroSegun(apuestas)).sum,
-            probabilidad
-          )
-        }
-    ))
+    DistribucionParaApuestas(unificarRepetidos(distribucionParaJugadas.distribucion
+        .map { case (jugadas, probabilidad) => (jugadas.map(_.dineroSegun(apuestas)).sum , probabilidad)}))
   }
-
 }
--- scala/src/main/scala/PlanDeJuego.scala
+++ scala/src/main/scala/PlanDeJuego.scala
@@ -1,8 +1,8 @@
 import Tipos.{Apuesta, Dinero, Probabilidad}
 
 case class PlanDeJuego(val nombre: String, val apuestas: List[Apuesta]){ //jugada sucesiva
-  def aplicar(montoInicial: Dinero): List[(Dinero, Probabilidad)] = ???/* {
-    apuestas.foldRight(DistribucionParaApuestas(List.empty))((apuesta, distribucion) => distribucion.combinar(apuesta))
+  def aplicar(montoInicial: Dinero): List[(Dinero, Probabilidad)] = ???/*{
+    apuestas.foldLeft(DistribucionParaApuestas(/*inserte semilla que tom sabe cual es*/))((apuesta, distribucion) => distribucion.combinar(apuesta))
   }*/
 
 }
