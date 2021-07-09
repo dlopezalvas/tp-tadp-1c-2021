4539117b3a2a125c8933cd0998ee958c29f642e0
araceli <asoffulto@frba.utn.edu.ar>
2021-07-09 12:12:50 -0300

    nombres tests y algunos mas

--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -3,6 +3,7 @@ import org.scalatest.matchers.should.Matchers._
 import org.scalatest.freespec.AnyFreeSpec
 
 class ProjectSpec extends AnyFreeSpec {
+  def roundProb(p: Double): Double = BigDecimal(p).setScale(4, BigDecimal.RoundingMode.HALF_EVEN).toDouble
 
   "Jugadas" - {
 
@@ -24,19 +25,22 @@ class ProjectSpec extends AnyFreeSpec {
 
       "Probabildad de que salga numero en la ruleta es de 1/37" in {
         val jugarAlNumero5: JugadaRuleta = NumeroJugado(5)
-        Ruleta.simularJugadas(List(jugarAlNumero5)).distribucion.filter(p => p._1.contains(jugarAlNumero5)) shouldBe List((List(jugarAlNumero5),1.0/37.0))
+        val simulacion: (List[Jugada], Probabilidad) = Ruleta.simularJugadas(List(jugarAlNumero5)).distribucion.filter(p => p._1.contains(jugarAlNumero5)).head
+        roundProb(simulacion._2) shouldBe roundProb(1.0/37.0)
       }
 
       "Probabilidad de que salga un color es 18/37" in {
         val jugarRojo: JugadaRuleta = ColorJugado(Color.Rojo)
         val apuesta: ApuestaRuleta = ApuestaRuleta(List((jugarRojo, 1.0)))
-        Ruleta.simularApuesta(apuesta).distribucion.filter(d => d._1 > 0) shouldBe List((2.0,18.0/37.0))
+        val simulacion: (Dinero, Probabilidad) = Ruleta.simularApuesta(apuesta).distribucion.filter(d => d._1 > 0).head
+         roundProb(simulacion._2) shouldBe roundProb(18.0/37.0)
       }
 
       "Probabilidad de que salga una docena es de 12/37" in {
         val jugarDocena: JugadaRuleta = DocenaJugada(1)
         val apuesta: ApuestaRuleta = ApuestaRuleta(List((jugarDocena, 1.0)))
-        Ruleta.simularApuesta(apuesta).distribucion.filter(d => d._1 > 0) shouldBe List((3.0,12.0/37.0))
+        val simulacion: (Dinero, Probabilidad) =Ruleta.simularApuesta(apuesta).distribucion.filter(d => d._1 > 0).head
+        roundProb(simulacion._2) shouldBe roundProb(12.0/37.0)
       }
     }
 
@@ -75,7 +79,7 @@ class ProjectSpec extends AnyFreeSpec {
     }
 
     "Jugadas Sucesivas" - {
-      "Si juego cara en la moneda y luego a un nro en la ruleta mis planes son" in {
+      "Si juego cara en la moneda y luego a un nro en la ruleta mis probabilidades son" in {
         val jugarACara:JugadaMoneda = JugadaMoneda(LadoMoneda.Cara)
         val jugarAlCero: JugadaRuleta = NumeroJugado(0)
 
@@ -83,12 +87,8 @@ class ProjectSpec extends AnyFreeSpec {
         val apostar15ACero:ApuestaRuleta = ApuestaRuleta(List((jugarAlCero,15.0)))
 
         val planDeJuego:PlanDeJuego = PlanDeJuego("monedaYRuleta",List(apostar10ACara,apostar15ACero))
-        //BigDecimal(0.5 * 1.0/37.0).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-        planDeJuego.aplicar(15.0) shouldBe List(
-          (550.0, 0.5 * 1.0/37.0),
-          (10.0,0.48648648648648607), //(10.0, 0.5 * 36.0/37.0),
-          (5.0, 0.5)
-        )
+        val resultadosRounded = planDeJuego.aplicar(15.0).map(p => roundProb(p._2))
+        resultadosRounded shouldBe List(roundProb(0.5 * 1.0/37.0), roundProb(0.5 * 36.0/37.0),0.5)
       }
 
       "Si juego un nro y luego un color mis probabilidades son" in {
@@ -99,15 +99,35 @@ class ProjectSpec extends AnyFreeSpec {
         val apostarANegro:ApuestaRuleta = ApuestaRuleta(List((juegoNegro,15.0)))
 
         val planDeJuego:PlanDeJuego = PlanDeJuego("numeroYColor",List(apostarAl16,apostarANegro))
+        val resultadosRounded = planDeJuego.aplicar(15.0).map(p => roundProb(p._2))
 
-        planDeJuego.aplicar(15.0) shouldBe List(
-          (5.0, 0.9729729729729721),
-          (350.0, 0.013878743608473342), //(1.0/37.0)*(18.0/37.0)), //TODO: revisar #rari
-          (380.0, (1.0/37.0)*(18.0/37.0))
-        )
+        resultadosRounded shouldBe List(roundProb(36.0/37.0),roundProb((1.0/37.0)*(19.0/37.0)),roundProb((1.0/37.0)*(18.0/37.0)))
       }
 
-      //TODO: agregar combinaciones  moneda-color   paridad-docena
+      "Si juego cara y luego un color mis probabilidades son" in  {
+        val jugarACara:JugadaMoneda = JugadaMoneda(LadoMoneda.Cara)
+        val juegoNegro = ColorJugado(Color.Negro)
+
+        val apostarACara:ApuestaMoneda = ApuestaMoneda(List((jugarACara,10.0)))
+        val apostarANegro:ApuestaRuleta = ApuestaRuleta(List((juegoNegro,15.0)))
+
+        val planDeJuego:PlanDeJuego = PlanDeJuego("caraYColor",List(apostarACara,apostarANegro))
+        val resultadosRounded = planDeJuego.aplicar(15.0).map(p => roundProb(p._2))
+
+        resultadosRounded shouldBe List(roundProb(0.5*19.0/37.0), roundProb(0.5*18.0/37.0),0.5)
+      }
+
+      "Si juego a paridad y luego a docena mis probabilidades son" in {
+        val jugarAPar: JugadaRuleta = ParidadJugada(Paridad.Par)
+        val jugarDocena: JugadaRuleta = DocenaJugada(1)
+        val apostarPar: ApuestaRuleta = ApuestaRuleta(List((jugarAPar, 10.0)))
+        val apostarDocena: ApuestaRuleta = ApuestaRuleta(List((jugarDocena, 10.0)))
+
+        val planDeJuego:PlanDeJuego = PlanDeJuego("paridadYDocena",List(apostarPar,apostarDocena))
+        val resultadosRounded = planDeJuego.aplicar(15.0).map(p => roundProb(p._2))
+
+        resultadosRounded shouldBe List(roundProb(19.0/37.0), roundProb(18.0/37.0*25.0/37.0), roundProb(18.0/37.0*12.0/37.0))
+      }
     }
 
     "Jugadores y Planes de Juego" - {
@@ -160,8 +180,26 @@ class ProjectSpec extends AnyFreeSpec {
 
         jugador.elegirPlanDeJuego(List(planDeJuego1, planDeJuego2)) shouldBe planDeJuego1
       }
+
+      "Permite crear jugadores a partir de nuevos criterios" in {
+        // al contrario que el jugador cauto elige la opción con mas probabilidades de perder (?
+        object criterioLoco extends CriterioJugador{
+          def apply(montoInicial: Dinero, distribuciones: List[DistribucionPlanDeJuego]): PlanDeJuego =
+            distribuciones.maxBy(_._2.filter(_._1 <= montoInicial).map(_._2).sum)._1
+        }
+
+        val apostarPocoACara: ApuestaMoneda = ApuestaMoneda(List((JugadaMoneda(LadoMoneda.Cara), 10.0)))
+        val apostarTodoAlUno: ApuestaRuleta = ApuestaRuleta(List((NumeroJugado(1), 100.0)))
+
+        val planDeJuego1:PlanDeJuego = PlanDeJuego("dosVecesCara",List(apostarPocoACara))
+        val planDeJuego2:PlanDeJuego = PlanDeJuego("dosVecesUno",List(apostarTodoAlUno))
+
+        val jugadorLoco:Jugador = Jugador(100.0, criterioLoco)
+
+        jugadorLoco.elegirPlanDeJuego(List(planDeJuego1, planDeJuego2)) shouldBe planDeJuego2
+      }
     }
 
-    //TODO: agregar otro jugador
+
   }
 }
